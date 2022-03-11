13be4bde08624d198190b54ce60b4f1e50945d93
nellafwork <nellafwork@gmail.com>
2022-03-11 17:26:41 +0300

    added request

--- app/Http/Controllers/Admin/Author/StoreController.php
+++ app/Http/Controllers/Admin/Author/StoreController.php
@@ -3,17 +3,15 @@
 namespace App\Http\Controllers\Admin\Author;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Author\StoreRequest;
 use App\Models\Author;
 use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'name' => 'string',
-            'surname' => 'string',
-        ]);
+        $data = $request->validated();
         Author::create($data);
 
         return redirect()->route('author.index');
